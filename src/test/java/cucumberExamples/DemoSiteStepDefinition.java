045a2ae95eb9a7c931076ccda1175c3347ab6adb
Jason-Fyfe <JFyfe@academytrainee.com>
2021-01-07 14:04:14 +0000

    Initial commit

---
+++ src/test/java/cucumberExamples/DemoSiteStepDefinition.java
@@ -0,0 +1,28 @@
+package cucumberExamples;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class DemoSiteStepDefinition
+{
+    @Given("^the correct web address$")
+    public void theCorrectWebAddress()
+    {
+    }
+
+    @When("^I register an account$")
+    public void iRegisterAnAccount()
+    {
+    }
+
+    @Then("^a new account is created$")
+    public void aNewAccountIsCreated()
+    {
+    }
+
+    @When("^I attempt to login$")
+    public void iAttemptToLogin()
+    {
+    }
+}
---
+++ src/test/java/cucumberExamples/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package cucumberExamples;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(monochrome = true, plugin = {"pretty", "html:target/cucumber-report.html"})
+public class RunCucumberTest
+{}
